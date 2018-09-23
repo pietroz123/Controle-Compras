258b3c241c64c9cc13a3ce7a2d5a0117020babe4
Pietro <pietroz@terra.com.br>
2018-09-23 01:01:52 -0300

    Um pouco do footer (precisa melhorar)

--- rodape.php
+++ rodape.php
@@ -1,5 +1,12 @@
             </div>
         </div>
+
+        <footer class="footer">
+            <div class="container">
+                <p class="text-muted">&copy; Pietro Zuntini Bonfim, 2018</p>
+            </div>
+        </footer>
+
     </body>
 
 </html>
