a6a3f1761dfeb91b5c66f2c4386dcb79e7888048
Pietro <pietroz@terra.com.br>
2018-09-30 01:00:49 -0300

    Generalização da função listar

--- listar-compras.php
+++ listar-compras.php
@@ -5,9 +5,9 @@
 
 <?php 
 
-    function listaCompras($conexao) {
+    function listar($conexao, $query) {
         $compras = array();
-        $resultado = mysqli_query($conexao, "select * from compras");
+        $resultado = mysqli_query($conexao, $query);
         while ($compra = mysqli_fetch_assoc($resultado)) {
             array_push($compras, $compra);
         }
@@ -30,7 +30,7 @@
     </thead>
 
     <?php
-        $compras = listaCompras($conexao);
+        $compras = listar($conexao, "select * from compras");
         foreach ($compras as $compra) :
     ?>
 
