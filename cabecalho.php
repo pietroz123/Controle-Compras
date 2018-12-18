c18178e3edfe38272ce073ef7a7d64d5505865d0
Pietro (desktop) <pietroz@terra.com.br>
2018-12-18 13:37:44 -0200

    Navbar collapse e CSS (em progresso)

--- cabecalho.php
+++ cabecalho.php
@@ -7,6 +7,7 @@
         <link type="text/css" href="css/compras.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Merriweather|Slabo+27px" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
     <body>
@@ -20,10 +21,23 @@
                 </div>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link" href="formulario-compra.php">Adicionar Compra</a></li>
-                        <li class="nav-item"><a class="nav-link" href="formulario-comprador.php">Adicionar Comprador</a></li>
-                        <li class="nav-item"><a class="nav-link" href="listar-compras.php">Compras</a></li>
-                        <li class="nav-item"><a class="nav-link" href="buscar.php">Buscar</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="formulario-compra.php">Adicionar Compra</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="formulario-comprador.php">Adicionar Comprador</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="listar-compras.php">Compras</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="buscar.php">Buscar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0);" class="nav-link icon" onclick="myFunction()">
+                                <i class="fa fa-bars"></i>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
