7f85bf6911c2275e19f8c9b989c1c02329e8623b
Pietro Zuntini Bonfim <pietroz@terra.com.br>
2018-12-21 00:13:30 -0200

    Algumas divs no buscar.php

--- buscar.php
+++ buscar.php
@@ -10,26 +10,35 @@
             Buscar compras
         </div>
         <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Palavra/frase chave</li>
-                <li class="list-group-item"><input class="form-control" type="text" name="texto"></li>
-                <li class="list-group-item">Data</li>
-                <li class="list-group-item">De: <input class="form-control" type="date" name="dataInicio"><br>Até: <input class="form-control" type="date" name="dataFim"></li>
-                <li class="list-group-item">Comprador</li>
-                <li class="list-group-item">
-                    <select class="custom-select" name="comprador" id="comprador">
-                        <option class="text-muted">Selecione uma Opção</option>
-                        <option value="0">Todos</option>
-                        <?php 
-                            $compradores = listar($conexao, "SELECT * FROM compradores");
-                            foreach ($compradores as $comprador) :
-                        ?>
-                                <option value="<?= $comprador['ID']; ?>"><?= $comprador['Nome']; ?></option>
-                        <?php
-                            endforeach;
-                        ?>
-                    </select>
-                </li>
+            <ul class="list-group">
+                <div class="div-palavra-chave">
+                    <li class="list-group-item">Palavra/frase chave</li>
+                    <li class="list-group-item">
+                        <input class="form-control palavra-chave" type="text" name="texto">
+                    </li>
+                </div>
+                <div class="div-data">
+                    <li class="list-group-item">Data</li>
+                    <li class="list-group-item">De: <input class="form-control" type="date" name="dataInicio"></li>
+                    <li class="list-group-item">Até: <input class="form-control" type="date" name="dataFim"></li>
+                </div>
+                <div class="div-comprador">
+                    <li class="list-group-item">Comprador</li>
+                    <li class="list-group-item">
+                        <select class="custom-select" name="comprador" id="comprador">
+                            <option class="text-muted">Selecione uma Opção</option>
+                            <option value="0">Todos</option>
+                            <?php 
+                                $compradores = listar($conexao, "SELECT * FROM compradores");
+                                foreach ($compradores as $comprador) :
+                            ?>
+                                    <option value="<?= $comprador['ID']; ?>"><?= $comprador['Nome']; ?></option>
+                            <?php
+                                endforeach;
+                            ?>
+                        </select>
+                    </li>
+                </div>
 
             </ul>
                 
