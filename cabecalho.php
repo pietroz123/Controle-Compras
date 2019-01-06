95dcf3d175a7107795b65ebad0d16249ab24c10a
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-01-05 22:06:32 -0200

    Roboto Slab no conteudo-prinicipal

--- cabecalho.php
+++ cabecalho.php
@@ -15,6 +15,7 @@
         
         <!-- Fontes de Texto -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Slabo+27px">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
     </head>
