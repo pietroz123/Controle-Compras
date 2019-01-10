a87bfd479bd71ef4ca40594886f5e63be60d03ef
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-01-10 14:35:06 -0200

    Inicio da estilizacao

---
+++ email-recuperacao.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather|Slabo+27px" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+    </head>
+    <body>
+
+        <style>
+            body {
+                background-color: #f2f2f2;
+                text-align: center;
+            }
+            h1 {
+                font-family: 'Merriweather', serif;
+                font-size: 20px;
+                background-color: #990000;
+                padding: 25px;
+                margin: 0;
+                color: white;
+                text-transform: uppercase;
+                border: 3px solid black;
+            }
+            .texto-email {
+                font-family: 'Roboto Slab', serif;
+                margin: 40px 40px 20px 40px;
+                font-size: 18px;
+            }
+        </style>
+
+        <h1>Informações para a recuperação da sua senha</h1>
+        <div class="texto-email">
+            <p>Recebemos uma requisição de recuperação de senha. O link para recuperar sua senha está logo abaixo. Se você não fez essa requisição, ignore este e-email.</p>
+        </div>
+    </body>
+</html>
