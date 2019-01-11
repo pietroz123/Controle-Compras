5a933f7f108de79b2b82629d7bad746b41825cf0
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-01-10 23:55:32 -0200

    Mais CSS no email

--- email-recuperacao.php
+++ email-recuperacao.php
@@ -11,13 +11,17 @@
         <style>
             body {
                 background-color: #f2f2f2;
-                text-align: center;
             }
             h1 {
+                position: fixed;
+                left: 0;
+                top: 0;
+                width: 99.5%;
+                text-align: center;
                 font-family: 'Merriweather', serif;
                 font-size: 20px;
                 background-color: #990000;
-                padding: 25px;
+                padding: 25px 0;
                 margin: 0;
                 color: white;
                 text-transform: uppercase;
@@ -25,14 +29,51 @@
             }
             .texto-email {
                 font-family: 'Roboto Slab', serif;
-                margin: 40px 40px 20px 40px;
+                margin: 120px 30px;
+                text-align: justify;
                 font-size: 18px;
+            }
+            .texto-email p {
+                margin-bottom: 15px;
+            }
+            footer {
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+            }
+            footer p {
+                font-family: 'Merriweather', serif;
+                font-size: 18px;
+                background-color: #990000;
+                padding: 25px;
+                margin: 0;
+                color: white;
+                border: 3px solid black;
+            }
+
+            @media only screen and (max-width: 550px) {
+                h1 {
+                    font-size: 18px;
+                    width: 98.5%;   
+                }
+                .texto-email {
+                    font-size: 14px;
+                }
+                footer p {
+                    font-size: 16px;
+                }
             }
         </style>
 
         <h1>Informações para a recuperação da sua senha</h1>
         <div class="texto-email">
             <p>Recebemos uma requisição de recuperação de senha. O link para recuperar sua senha está logo abaixo. Se você não fez essa requisição, ignore este e-email.</p>
+            <p>Aqui está o link de recuperação da senha:<br><a href="www.pietroz.tech">www.pietroz.tech</a></p>
         </div>
+
+        <footer>
+            <p>Pietro Zuntini Bonfim &copy;</p>
+        </footer>
     </body>
 </html>
