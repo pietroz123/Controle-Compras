a3ed599f39cce76ba5641d5a7ccc465963059060
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-01-29 23:12:53 -0200

    Arrumando a largura do select2

--- modal-criar-grupo.php
+++ modal-criar-grupo.php
@@ -5,26 +5,30 @@
                 <h3>Criação de Grupo</h3>
                 <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
             </div>
-            <div class="modal-body">
-                <form name="adicionar-username" id="adicionar-username" action="scripts/adicionar-grupo.php" method="post">
+            <form name="adicionar-username" id="adicionar-username" action="scripts/adicionar-grupo.php" method="post">
+                <div class="modal-body">
                     <div class="grid">
                         <div class="row">
-                            <div class="col-sm-4">Nome:</div>
+                            <div class="col-sm-4" style="font-weight: bold;">Nome:</div>
                             <div class="col-sm-8"><input type="text" name="nome-grupo" class="form-control" placeholder="Nome do grupo"></div>
                         </div>
+                        <hr>
                         <div class="row">
+                            <div class="col-sm" style="font-weight: bold;">Usuários:</div>
+                        </div>
+                        <div class="row mt-2">
                             <div class="col-sm">
-                                <select class="form-control input-usuario" name="usernames[]" multiple="multiple">
-                                    
+                                <select class="form-control input-usuario" name="usernames[]" multiple="multiple" style="width: 100%;">
+
                                 </select>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="modal-footer">
                     <button type="submit" name="submit-criar-grupo" id="submit-criar-grupo" class="btn btn-blue">Criar grupo</button>
-                </form>
-            </div>
-            <div class="modal-footer">
-            </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>    
