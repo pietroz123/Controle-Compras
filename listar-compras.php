abb6b0bce333c3d828352d180311dcfbb067363f
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-02-06 21:46:50 -0200

    Fix: posicionamento das tags

--- listar-compras.php
+++ listar-compras.php
@@ -55,29 +55,30 @@
         $i++;
 ?>
 
-<a role="button" class="link-cartao-grupo" id-grupo="<?= $grupo['ID']; ?>">
-    <div class="cartao-grupo">
-        <div class="row">
-            <div class="col-sm-3 col cartao-grupo-imagem indigo">
-                <i class="fa fa-users fa-2x grupo-imagem rounded-circle"></i>
-            </div>
-            <div class="col-sm-9 col cartao-grupo-informacoes white">
-                <div class="row">
-                    <div class="col">
-                        <h6 class="cartao-grupo-nome"><?= $grupo['Nome']; ?></h6>
+    <a role="button" class="link-cartao-grupo" id-grupo="<?= $grupo['ID']; ?>">
+        <div class="cartao-grupo">
+            <div class="row">
+                <div class="col-sm-3 col cartao-grupo-imagem indigo">
+                    <i class="fa fa-users fa-2x grupo-imagem rounded-circle"></i>
+                </div>
+                <div class="col-sm-9 col cartao-grupo-informacoes white">
+                    <div class="row">
+                        <div class="col">
+                            <h6 class="cartao-grupo-nome"><?= $grupo['Nome']; ?></h6>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col" id="data-criacao">Data de Criação</div>
+                        <div class="col-sm-6 col" id="data"><?= date("d/m/Y h:m", strtotime($grupo['Data_Criacao'])); ?></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col" id="numero-membros">Número de membros</div>
+                        <div class="col-sm-6 col" id="numero"><?= $grupo['Numero_Membros']; ?></div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm-6 col" id="data-criacao">Data de Criação</div>
-                    <div class="col-sm-6 col" id="data"><?= date("d/m/Y h:m", strtotime($grupo['Data_Criacao'])); ?></div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-6 col" id="numero-membros">Número de membros</div>
-                    <div class="col-sm-6 col" id="numero"><?= $grupo['Numero_Membros']; ?></div>
-                </div>
-            </div>
-        </div>  
-    </div>
+            </div>  
+        </div>
+    </a>
 
 <?php
     }
@@ -87,8 +88,7 @@
 <?php
     }
 ?>
-    </div>
-</a>
+</div>
 
 <div class="cartoes">
     <div class="cartao-novo">
