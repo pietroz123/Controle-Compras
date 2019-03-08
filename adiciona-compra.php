7e0af4dcb91147b9a083a476390ce4104060f33c
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-03-08 01:43:37 -0300

    Corta, envia e salva corretamente

--- adiciona-compra.php
+++ adiciona-compra.php
@@ -30,47 +30,78 @@
     $imagem_erro        = $_FILES['imagem']['error'];
     $imagem_tipo        = $_FILES['imagem']['type'];
 
-    // Inicializa o novo nome com 'null'
-    $novo_nome = '';
+    $imagem_cortada     = $_POST['imagem-cortada'];
 
-    // Recupera a extensão do arquivo (para verificar se é JPG ou PNG)
-    $extensao = explode('.', $imagem_nome); // Divide a string e pega apenas a parte a partir do '.'
-    $ext = strtolower(end($extensao));      // Converte tudo pra letra minuscula e pega apenas depois do '.'
+    if (empty($imagem_cortada)) {
 
-    // Extensões permitidas
-    $permitido = array('jpg', 'jpeg', 'png');
-
-    // Verifica se uma imagem foi enviada
-    if (is_uploaded_file($_FILES['imagem']['tmp_name'])) {
-        // Verifica se a extensão é uma imagem
-        if (in_array($ext, $permitido)) {
-            // Verifica se não houve erros
-            if ($imagem_erro === 0) {
-                
-                $novo_nome = uniqid('') . "-" . $observacoes . "-" . $data . "." . $ext;
-                
-                // Destino do imagem
-                $destino = "../private/uploads/compras/" . $novo_nome;
-
-                // Armazena a imagem
-                move_uploaded_file($imagem_nome_tmp, $destino);
-
-                $novo_destino = comprimir($destino, "../private/uploads/compras/" . $novo_nome, 20);
-                move_uploaded_file($destino, $novo_destino);
-        
-                $_SESSION['success'] = "Imagem salva com sucesso.";
-            }
-            else { // Erro no upload da imagem
-                $_SESSION['danger'] = "Ocorreu um erro no upload da imagem!"; 
-            }
-        }
-        else { // Não está nas extensões permitidas
-            $_SESSION['danger'] = "Essa extensão não é permitida!";
-        }
-    }
-    else { // $novo_nome é vazio
         $_SESSION['info'] = "Nenhuma imagem selecionada.";
+
     }
+    else {
+
+        // Recupera a extensão do arquivo (para verificar se é JPG ou PNG)
+        $extensao = explode('.', $imagem_nome); // Divide a string e pega apenas a parte a partir do '.'
+        $ext = strtolower(end($extensao));      // Converte tudo pra letra minuscula e pega apenas depois do '.'
+
+        list(, $imagem_cortada)      = explode(',', $imagem_cortada);
+        $imagem_cortada = base64_decode($imagem_cortada);
+        
+        $novo_nome = uniqid('') . "-" . $observacoes . "-" . $data . "." . $ext;
+        
+        // Destino do imagem
+        $destino = "../private/uploads/compras/" . $novo_nome;
+
+        file_put_contents($destino, $imagem_cortada);
+        $novo_destino = comprimir($destino, "../private/uploads/compras/" . $novo_nome, 20);
+        move_uploaded_file($destino, $novo_destino);
+
+        $_SESSION['success'] = "Imagem salva com sucesso.";
+    
+    }
+
+
+    // Sem Cropper JS
+    // // Inicializa o novo nome com 'null'
+    // $novo_nome = '';
+    
+    // // Recupera a extensão do arquivo (para verificar se é JPG ou PNG)
+    // $extensao = explode('.', $imagem_nome); // Divide a string e pega apenas a parte a partir do '.'
+    // $ext = strtolower(end($extensao));      // Converte tudo pra letra minuscula e pega apenas depois do '.'
+    
+    // // Extensões permitidas
+    // $permitido = array('jpg', 'jpeg', 'png');
+    
+    // // Verifica se uma imagem foi enviada
+    // if (is_uploaded_file($_FILES['imagem']['tmp_name'])) {
+    //     // Verifica se a extensão é uma imagem
+    //     if (in_array($ext, $permitido)) {
+    //         // Verifica se não houve erros
+    //         if ($imagem_erro === 0) {
+                
+    //             $novo_nome = uniqid('') . "-" . $observacoes . "-" . $data . "." . $ext;
+                
+    //             // Destino do imagem
+    //             $destino = "../private/uploads/compras/" . $novo_nome;
+
+    //             // Armazena a imagem
+    //             move_uploaded_file($imagem_nome_tmp, $destino);
+
+    //             $novo_destino = comprimir($destino, "../private/uploads/compras/" . $novo_nome, 20);
+    //             move_uploaded_file($destino, $novo_destino);
+        
+    //             $_SESSION['success'] = "Imagem salva com sucesso.";
+    //         }
+    //         else { // Erro no upload da imagem
+    //             $_SESSION['danger'] = "Ocorreu um erro no upload da imagem!"; 
+    //         }
+    //     }
+    //     else { // Não está nas extensões permitidas
+    //         $_SESSION['danger'] = "Essa extensão não é permitida!";
+    //     }
+    // }
+    // else { // $novo_nome é vazio
+    //     $_SESSION['info'] = "Nenhuma imagem selecionada.";
+    // }
 
 
     mostra_alerta("danger");
--- formulario-compra.php
+++ formulario-compra.php
@@ -67,6 +67,8 @@
                     </div>
                 </div>
                 <hr>
+
+                <!-- Sem Cropper JS -->
                 <!-- <div class="row">
                     <div class="col-lg-4">Imagem (Opcional)</div>
                     <div class="col-lg-8">
@@ -95,7 +97,7 @@
                                     <span>Selecione uma imagem</span>
                                 </label>
                                 <div id="canvas">
-                                    <img id="imagem-nota" alt="Imagem nota">
+
                                 </div>
                             </div>
                             <div class="modal-footer">
@@ -104,13 +106,13 @@
                         </div>
                     </div>
                 </div>
-
+                <input type="hidden" name="imagem-cortada" value="">
 
 
                 <hr>
             </div>
             
-            <div><button type="submit" name="submit-form-compra" class="btn btn-primary btn-block align-content-center">Adicionar</button></div>
+            <div><button type="submit" name="submit-form-compra" id="btn-adicionar" class="btn btn-primary btn-block align-content-center">Adicionar</button></div>
 
         </form>
 
@@ -118,34 +120,12 @@
 
 <script>
 
-    // // https://tympanus.net/codrops/2015/09/15/styling-customizing-file-inputs-smart-way/
-    // var input = $('#input-imagem');
-    // Array.prototype.forEach.call( input, function( input )
-    // {
-    //     var label	 = input.nextElementSibling,
-    //         labelVal = label.innerHTML;
-
-    //     input.addEventListener( 'change', function( e )
-    //     {
-    //         var fileName = '';
-    //         if( this.files && this.files.length > 1 && this.files[0].type.match(/^image\//))
-    //             fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{numero}', this.files.length );
-    //         else
-    //             fileName = e.target.value.split( '\\' ).pop();
-
-    //         if( fileName )
-    //             label.querySelector( 'span' ).innerHTML = fileName;
-    //         else
-    //             label.innerHTML = labelVal;
-    //     });
-    // });
-
 
     // =======================================================
     // Para uso do Cropper.js
     // =======================================================
 
-    var canvas = $('#canvas');
+    var cropper;
 
     $('#input-imagem').on( 'change', function(e){
         if (this.files && this.files[0]) {
@@ -153,22 +133,25 @@
             var arquivo = arquivos[0];
             if ( arquivo.type.match(/^image\//) ) {
 
+                // Cria o elemento da imagem
+                $('#canvas').append('<img id="imagem-nota" alt="Imagem nota">');
+
                 var imagem = document.getElementById("imagem-nota");
                 $('#imagem-nota').attr("src", window.URL.createObjectURL(arquivo));
 
                 // Cria o Cropper
-                var cropper = new Cropper(imagem, {
+                cropper = new Cropper(imagem, {
                     aspectRatio: NaN
                 });
 
                 // Adiciona o botao para cortar
                 $('#modal-upload-imagem .modal-footer').append('<input type="button" class="btn btn-light btn-cortar" value="Cortar">');
 
-                var label	 = $('#input-imagem').siblings('label');
-                var labelVal = label.val();
-                
 
                 // Adiciona o nome do arquivo ao label do input
+                var label	 = $('#input-imagem').siblings('label');
+                var labelVal = label.val();
+
                 var fileName = '';
                 if( this.files && this.files.length > 1 )
                     fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{numero}', this.files.length );
@@ -183,12 +166,15 @@
 
                 // Coloca a imagem cortada no canvas
                 $('.btn-cortar').click(function() {
-                    // Get a string base 64 data url
+                    
+                    // Recupera a imagem em base64
                     var croppedImageDataURL = cropper.getCroppedCanvas().toDataURL("image/png");
+                    $('input[type="hidden"][name="imagem-cortada"]').val(croppedImageDataURL);
+                    
+                    // Adiciona a imagem cortada ao canvas
                     $('#canvas').html('<img src="'+croppedImageDataURL+'" alt="Imagem cortada">');
                     arquivo = croppedImageDataURL;
                 });
-
 
             }
             else {
