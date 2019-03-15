bdf8a5bcdb33a1e72efbcb448bcf021e4b969c6d
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-03-15 14:56:53 -0300

    Revert antes input obs select2

--- formulario-compra.php
+++ formulario-compra.php
@@ -22,10 +22,7 @@
             
                 <div class="row">
                     <div class="col-lg-4">Observações</div>
-                    <div class="col-lg-8">
-                        <select class="form-control" id="select2-observacoes" name="observacoes-select" style="width: 100%;"></select>
-                        <input class="form-control" id="input-observacoes" type="hidden" name="observacoes">
-                    </div>
+                    <div class="col-lg-8"><input class="form-control" type="text" name="observacoes"></div>
                 </div>
                 <hr>
                 <div class="row">
@@ -70,6 +67,18 @@
                     </div>
                 </div>
                 <hr>
+
+                <!-- Sem Cropper JS -->
+                <!-- <div class="row">
+                    <div class="col-lg-4">Imagem (Opcional)</div>
+                    <div class="col-lg-8">
+                        <input class="form-control-file" type="file" id="input-imagem" name="imagem" data-multiple-caption="{numero} arquivos selecionados" multiple>
+                        <label for="input-imagem">
+                            <i class="far fa-file-image"></i>
+                            <span>Selecione uma imagem</span>
+                        </label>
+                    </div>
+                </div> -->
 
 
                 <div class="list-inline">
@@ -125,40 +134,6 @@
         // =======================================================
 
         $('#input-valor').mask('000000000000000.00', {reverse: true});
-
-
-        // =======================================================
-        // Realiza a busca por observações já existentes
-        // =======================================================
-
-        $('#select2-observacoes').select2({
-            placeholder: 'Digite o nome do produto',
-            ajax: {
-                url: "scripts/busca-observacao.php",
-                type: "post",
-                dataType: "json",
-                delay: 250,
-                data: function(params) {
-                    return {
-                        busca: "sim",
-                        texto: params.term
-                    };
-                },
-                processResults: function(data) {
-                    return {
-                        results: data
-                    };
-                },
-                cache: true
-            },
-            allowClear: true
-        });
-
-        $('#select2-observacoes').change(function() {
-            // var selecionado = $('#select2-observacoes').select2('data')[0].text;
-            var selecionado = $('.select2-selection__rendered').attr('title');
-            $('#input-observacoes').val(selecionado);            
-        });
 
     });
 
