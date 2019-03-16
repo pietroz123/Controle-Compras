0394bc46e68ca56d16c388cc574f6ec4d69c2bb4
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-03-15 22:57:39 -0300

    #14: Nova busca levando em consideracao os grupos

--- scripts/busca-observacao.php
+++ scripts/busca-observacao.php
@@ -1,16 +1,91 @@
 <?php
 
 
+function grupos($conexao, $username) {
+    $grupos = "SELECT gu.ID_Grupo
+        FROM grupo_usuarios gu
+        WHERE gu.Username = '{$username}'";
+    $resultado = mysqli_query($conexao, $grupos);
+    if (mysqli_num_rows($resultado) > 0) {
+        $ids_grupos_usuario = array();
+        while ($id_grupo_usuario = mysqli_fetch_assoc($resultado)) {
+            array_push($ids_grupos_usuario, $id_grupo_usuario);
+        }
+        return $ids_grupos_usuario;
+    }
+    else
+        return null;
+}
+
+function string_or_grupos($ids_grupos_usuario) {
+    $string_ids_grupos = "";
+    foreach ($ids_grupos_usuario as $id_grupo_usuario) {
+        if ($id_grupo_usuario != end($ids_grupos_usuario))
+            $string_ids_grupos .= "gu.ID_Grupo = " . $id_grupo_usuario['ID_Grupo'] . " OR ";
+        else
+            $string_ids_grupos .= "gu.ID_Grupo = " . $id_grupo_usuario['ID_Grupo'];
+    }
+    return $string_ids_grupos;
+}
+
+function usuarios($conexao, $username, $string_ids_grupos) {
+    $grupo_usuarios = "SELECT DISTINCT gu.Username
+                FROM grupo_usuarios gu
+                WHERE (".$string_ids_grupos.")";
+    $resultado = mysqli_query($conexao, $grupo_usuarios);
+    $usuarios = array();
+    while ($usuario = mysqli_fetch_assoc($resultado)) {
+        array_push($usuarios, $usuario);
+    }
+    return $usuarios;
+}
+
+function string_or_usuarios($usuarios) {
+    $string_usuarios = "";
+    foreach ($usuarios as $usuario) {
+        if ($usuario != end($usuarios))
+            $string_usuarios .= "u.Usuario = '" . $usuario['Username'] . "' OR ";
+        else
+            $string_usuarios .= "u.Usuario = '" . $usuario['Username'] . "'";
+    }
+    return $string_usuarios;
+}
+
+
     if (isset($_POST['busca']) && $_POST['busca'] == 'sim') {
 
         include $_SERVER['DOCUMENT_ROOT'].'/config/sessao.php';
         include $_SERVER['DOCUMENT_ROOT'].'/database/conexao.php';
 
         $observacao = strip_tags($_POST['texto']);
-        
-        /* Busca os usuários no Banco de Dados (menos o próprio usuário) */
 
-        $sql = "SELECT DISTINCT `Observacoes` FROM `compras` WHERE `Observacoes` LIKE ?";
+        // Seleciona todos os grupos do usuario admin
+        $ids_grupos_usuario = grupos($conexao, $_SESSION['login-username']);
+        if ($ids_grupos_usuario != null) {
+
+            $string_ids_grupos = string_or_grupos($ids_grupos_usuario);
+
+            // Seleciona todos os usuarios pertencentes a esses grupos
+            $usuarios = usuarios($conexao, $_SESSION['login-username'], $string_ids_grupos);
+
+            $string_usuarios = string_or_usuarios($usuarios);
+
+            $sql = "SELECT DISTINCT `Observacoes` FROM `compras` c WHERE `Observacoes` LIKE ? AND c.Comprador_ID IN (
+                        SELECT c.ID
+                        FROM compradores c
+                        JOIN usuarios u on c.Email = u.Email
+                        WHERE ".$string_usuarios."
+                    )";
+        }
+        else {
+            $sql = "SELECT DISTINCT `Observacoes` FROM `compras` c WHERE `Observacoes` LIKE ? AND c.Comprador_ID IN (
+                        SELECT c.ID
+                        FROM compradores c
+                        JOIN usuarios u on c.Email = u.Email
+                        WHERE u.Usuario = '{$_SESSION['login-username']}'
+                    )";
+        }
+
         $stmt = mysqli_stmt_init($conexao);
         
         if (!mysqli_stmt_prepare($stmt, $sql)) {
