ab05005968782db6889a6b9e35b5fa1be6f9f326
Pietro Zuntini Bonfim <pietroz@terra.com.br>
2019-05-13 20:46:36 -0300

    Inicio do AJAX

--- scripts/recuperar-dados-relatorios.php
+++ scripts/recuperar-dados-relatorios.php
@@ -10,6 +10,24 @@
             // Recuperar as datas e valores de compras
             case 'datas-compras':
 
+                $sql = "SELECT c.Data, c.Valor FROM compras c";
+                $compras = array();
+                $retorno = array();
+
+                $resultado = mysqli_query($conexao, $sql);
+                if ($resultado) {
+                    if (mysqli_num_rows($resultado) > 0) {
+                        while ($compra = mysqli_fetch_assoc($resultado)) {
+                            array_push($compras, $compra);
+                        }
+                    }
+                }
+
+                $retorno['compras'] = $compras;
+                $retorno['qtd_compras'] = count($compras);
+
+                echo json_encode($retorno);
+
                 break;
             
             default:
