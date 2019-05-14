71db78ad023584e68aefe5b099b85508bb34f22f
Pietro Zuntini - Desktop <pietrozuntini@gmail.com>
2019-05-13 22:22:45 -0300

    Versão orientada a datas do gráfico (decidir)

--- relatorios.php
+++ relatorios.php
@@ -37,7 +37,6 @@
     });
 
     function criar_grafico_compras(dados) {
-        
 
         // Themes begin
         am4core.useTheme(am4themes_animated);
@@ -84,6 +83,79 @@
 
         //chart.scrollbarY = new am4core.Scrollbar();
         chart.scrollbarX = new am4core.Scrollbar();
+
+    }
+
+    function criar_grafico_compras_v2(dados) {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+        var chart = am4core.create("grafico-compras", am4charts.XYChart);
+
+        var data = [];
+        for(let i = 0; i < dados.length; i++) {
+
+            // Criação da data da compra
+            var data_compra = dados[i].Data;
+
+            // Criação do valor da compra
+            var valor_compra = dados[i].Valor;
+
+            data.push({date: data_compra, value: valor_compra});
+        }
+
+        chart.data = data;
+
+        // Set input format for the dates
+        chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+
+        // Create axes
+        var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+        // Create series
+        var series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.valueY = "value";
+        series.dataFields.dateX = "date";
+        series.tooltipText = "{value}"
+        series.strokeWidth = 2;
+        series.minBulletDistance = 15;
+
+        // Drop-shaped tooltips
+        series.tooltip.background.cornerRadius = 20;
+        series.tooltip.background.strokeOpacity = 0;
+        series.tooltip.pointerOrientation = "vertical";
+        series.tooltip.label.minWidth = 40;
+        series.tooltip.label.minHeight = 40;
+        series.tooltip.label.textAlign = "middle";
+        series.tooltip.label.textValign = "middle";
+
+        // Make bullets grow on hover
+        var bullet = series.bullets.push(new am4charts.CircleBullet());
+        bullet.circle.strokeWidth = 2;
+        bullet.circle.radius = 4;
+        bullet.circle.fill = am4core.color("#fff");
+
+        var bullethover = bullet.states.create("hover");
+        bullethover.properties.scale = 1.3;
+
+        // Make a panning cursor
+        chart.cursor = new am4charts.XYCursor();
+        chart.cursor.behavior = "panXY";
+        chart.cursor.xAxis = dateAxis;
+        chart.cursor.snapToSeries = series;
+
+        // Create vertical scrollbar and place it before the value axis
+        chart.scrollbarY = new am4core.Scrollbar();
+        chart.scrollbarY.parent = chart.leftAxesContainer;
+        chart.scrollbarY.toBack();
+
+        // Create a horizontal scrollbar with previe and place it underneath the date axis
+        chart.scrollbarX = new am4charts.XYChartScrollbar();
+        chart.scrollbarX.series.push(series);
+        chart.scrollbarX.parent = chart.bottomAxesContainer;
 
     }
 
